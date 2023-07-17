adc855018424ffe8b19c749c4ba88ffc2a2943ed
exile213 <exilegamer3@gmail.com>
2023-07-17 22:00:20 +0800

    Add javacript script and css
    practiced some javascript

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var sayMyName = function(name){
+    alert('My name is: '+ name);
+}
+
+var car ={ //this is an object or dictionary in python
+    make: 'VW',
+    type: 'Polo',
+    color: 'blue',
+    isTurnedOn: false,
+    numberOfWheels: 4,
+    seats: ['seat 1', 'seat 2','seat 3', 'seat 4'],
+    turnOn: function(){
+        this.isTurnedOn =true;
+        //this refers to the car object so car.isTurnOn
+    },
+    fly: function() {
+        alert('fly')
+        //calling a function without parenthesis is just referencing it
+        //calling it with parenthesis is executing the function
+    },
+    switchCar: function (isOn){
+        console.log('turn car '+ isOn)
+        if (isOn == true){
+            this.isTurnedOn = true;
+        }
+        else{
+            this.isTurnedOn =false;
+        }
+    }
+};
+console.log('hello there friends')
