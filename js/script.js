784296d8cbd2c4df7baec99d9b1a63d1101d253e
exile213 <exilegamer3@gmail.com>
2023-07-22 20:01:42 +0800

    Practiced Css transition and css,html and integrations with js functions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
-var sayMyName = function(name){
-    alert('My name is: '+ name);
+
+var doCoolStuff = function () {
+    var currentClassName = document.getElementById('cool').className;
+    //changes class name 
+    if(currentClassName =='cool'){
+        document.getElementById('cool').className = 'cool red';
+    }
+    else{
+        document.getElementById('cool').className = 'cool';
+    }
 }
 
-var car ={ //this is an object or dictionary in python
+
+var sayMyName = function (name) {
+    alert('My name is: ' + name);
+}
+
+var car = { //this is an object or dictionary in python
     make: 'VW',
     type: 'Polo',
     color: 'blue',
     isTurnedOn: false,
     numberOfWheels: 4,
-    seats: ['seat 1', 'seat 2','seat 3', 'seat 4'],
-    turnOn: function(){
-        this.isTurnedOn =true;
+    seats: ['seat 1', 'seat 2', 'seat 3', 'seat 4'],
+    turnOn: function () {
+        this.isTurnedOn = true;
         //this refers to the car object so car.isTurnOn
     },
-    fly: function() {
+    fly: function () {
         alert('fly')
         //calling a function without parenthesis is just referencing it
         //calling it with parenthesis is executing the function
     },
-    switchCar: function (isOn){
-        console.log('turn car '+ isOn)
-        if (isOn == true){
+    switchCar: function (isOn) {
+        console.log('turn car ' + isOn)
+        if (isOn == true) {
             this.isTurnedOn = true;
         }
-        else{
-            this.isTurnedOn =false;
+        else {
+            this.isTurnedOn = false;
         }
     }
 };
